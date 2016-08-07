2456b2935bc55972ea681164e87011a272c47e45
qjpcpu <qjpcpu@gmail.com>
2016-08-07 22:55:10 +0800

    remove contrl

--- main.go
+++ main.go
@@ -178,7 +178,7 @@ func drawUI() {
 			} else if kb.KeyStr == "<space>" {
 				appendQuery(" ")
 			} else {
-				matched, _ := regexp.MatchString(`<.+>`, kb.KeyStr)
+				matched, _ := regexp.MatchString(`<.+>|C\-`, kb.KeyStr)
 				if !matched {
 					appendQuery(kb.KeyStr)
 				}
